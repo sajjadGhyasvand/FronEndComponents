f489a5a2a049d9289ad3ad66444562634932234b
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2024-05-21 14:56:52 +0330

    404 page

---
+++ JS Components/003-404NotFoundPage/js/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('#container');
+
+
+window.onmousemove = function(e){
+    let x = e.clientX / 5; 
+    let y = e.clientY / 5;
+    
+    container.style.backgroundPositionX = x + "px";
+    container.style.backgroundPositionY = y + "px";
+
+}
