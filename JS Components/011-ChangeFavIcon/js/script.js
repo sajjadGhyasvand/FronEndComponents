b15e2defce3cfd236517cdc9c1eb6543c2bf4253
sajjadGhyasvand <Sajjadgh1995@hotmail.com>
2024-07-19 15:08:45 +0330

    ChangeFavIcon

---
+++ JS Components/011-ChangeFavIcon/js/script.js
@@ -0,0 +1,11 @@
+const favicon = document.getElementById('favicon');
+console.log(favicon)
+
+
+function toRed(){
+    favicon.setAttribute('href', 'img/favicon.png');
+}
+
+function toGreen(){
+    favicon.setAttribute('href', 'img/faviconTwo.png');
+}
