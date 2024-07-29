498c3565c9013bfe8bfc08764207cd7f24acb9c9
sajjadGh1995@hotmail.com <137422sajjad>
2024-07-29 13:49:23 +0330

    skew Header

---
+++ JS Components/013-HeaderChangeShape/js/script.js
@@ -0,0 +1,6 @@
+let skewed = document.querySelector('.angel');
+
+window.addEventListener('scroll',function(){
+    let value = -10 + window.scrollY/60;
+    skewed.style.transform = "skewY("+value+"deg)";
+})
